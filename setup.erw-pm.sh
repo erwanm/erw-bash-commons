461e0dbdeb7c936d5fc58dbbc47132f01e107fd0
Erwan Moreau <moreaue@scss.tcd.ie>
2016-05-19 16:45:09 +0100

    reorganized dir structure

--- setup.erw-pm.sh
+++ setup.erw-pm.sh
@@ -4,4 +4,5 @@
 # Remark: special case because this is the project which contains the erw-pm system.
 #
 #echo "debug: in erw-bash-commons setup" 1>&2
-addToEnvVar "$PWD" PATH ":"
+addToEnvVar "$PWD/lib" PATH ":"
+addToEnvVar "$PWD/bin" PATH ":"
